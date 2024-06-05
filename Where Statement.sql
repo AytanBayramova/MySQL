38e94dca3b7230ea9f264aef43fefa02901fe62e
Aytan Bayramova <112419467+AytanBayramova@users.noreply.github.com>
2024-06-05 23:17:48 +0400

    Update Where Statement.sql

--- Where Statement.sql
+++ Where Statement.sql
@@ -1,14 +1,4 @@
-#WHERE Clause:
-#-------------
-#The WHERE clause is used to filter records (rows of data)
 
-#It's going to extract only those records that fulfill a specified condition.
-
-# So basically if we say "Where name is = 'Alex' - only rows were the name = 'Alex' will return
-# So this is only effecting the rows, not the columns
-
-
-#Let's take a look at how this looks
 SELECT *
 FROM employee_salary
 WHERE salary > 50000;
